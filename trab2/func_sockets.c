96f3ca79ab4f66a5344386566e9ec0a78c5ac9fc
freixo2004 <119111566+freixo2004@users.noreply.github.com>
2025-05-08 12:33:46 +0100

    Update func_sockets.c

--- trab2/func_sockets.c
+++ trab2/func_sockets.c
@@ -1,6 +1,29 @@
-int tcp_server_socket_init (int serverPort);
-int tcp_server_socket_accept (int serverSocket);
-int tcp_client_socket_init (const char *host, int port);
-int un_server_socket_init (const char *serverEndPoint);
-int un_server_socket_accept (int serverSocket);
-int un_client_socket_init (const char *serverEndPoint);
+int tcp_server_socket_init (int serverPort){
+
+  
+}
+
+int tcp_server_socket_accept (int serverSocket){
+
+  
+}
+
+int tcp_client_socket_init (const char *host, int port){
+
+  
+}
+
+int un_server_socket_init (const char *serverEndPoint){
+
+  
+}
+
+int un_server_socket_accept (int serverSocket){
+
+  
+}
+
+int un_client_socket_init (const char *serverEndPoint){
+
+  
+}
