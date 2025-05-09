abb6a3325096172fa0993427aacccab23101a28f
freixo2004 <119111566+freixo2004@users.noreply.github.com>
2025-05-09 11:27:50 +0100

    Update func_sockets.h

--- trab2/func_sockets.h
+++ trab2/func_sockets.h
@@ -1,6 +1,11 @@
+#ifndef FUNC_SOCKETS_H
+#define FUNC_SOCKETS_H
+
 int tcp_server_socket_init (int serverPort);
 int tcp_server_socket_accept (int serverSocket);
 int tcp_client_socket_init (const char *host, int port);
 int un_server_socket_init (const char *serverEndPoint);
 int un_server_socket_accept (int serverSocket);
 int un_client_socket_init (const char *serverEndPoint);
+
+#endif
