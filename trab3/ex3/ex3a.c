36d88a4c7a1ffeebcc0d67926021c56f2fb073b6
freixo2004 <119111566+freixo2004@users.noreply.github.com>
2025-05-26 17:27:50 +0100

    Update ex3a.c

--- trab3/ex3/ex3a.c
+++ trab3/ex3/ex3a.c
@@ -7,7 +7,8 @@
 
 const int N_THREADS = 2;
 
-void norm_min_max_and_classify_parallel(int v[], size_t v_sz, int nThreads){
+//void norm_min_max_and_classify_parallel(int v[], size_t v_sz, int nThreads){
+void norm_min_max_and_classify_parallel(int v[], size_t v_sz){
 //Criar N_THREADS
 
 
@@ -24,18 +25,38 @@ void norm_min_max_and_classify_parallel(int v[], size_t v_sz, int nThreads){
 
 
 //"transformar" o vetor em um de 0 a 100 e calcular a média
-
+int sum = 0;
 for(int i = 0; i < v_sz ; i++){
 
     v[i] = (v[i] – min) * 100 / (max-min);
+    sum += v[i];
 
 }
+//DEBUG
+for(int i = 0; i < v_sz ; i++){
+    printf("%d,",v[i]);
+
+}
+
+
+
+int media = sum/v_sz;
+//DEBUG
+printf("media = \n%d\n", media);
 //atribuir o valor 1 se o valor for maior ou igual que a média e 0 caso contrário 
+for(int i = 0; i < v_sz ; i++){
 
+    if(v[i]>=media) v[i] = 1;
+    else v[i] = 0;
 
 }
+//DEBUG
+for(int i = 0; i < v_sz ; i++){
+    printf("%d,",v[i]);
 
-
+}
+    
+}
 
 int main(){
 
@@ -43,6 +64,7 @@ int v[] = {10,20,30,40,50};
 
 int size = sizeof (v) / sizeof (int);
 
+norm_min_max_and_classify_parallel(v, size);
 //DEBUG
 printf ("size = %d\n", size);
 
