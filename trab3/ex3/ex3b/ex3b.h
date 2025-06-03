4c6408cfba2dba33f583310e84cfae4dbf0ded26
Rodrigo Roussado Viegas <r.roussado2@gmail.com>
2025-06-03 16:52:51 +0100

    Update ex3b.h

--- trab3/ex3/ex3b/ex3b.h
+++ trab3/ex3/ex3b/ex3b.h
@@ -7,3 +7,8 @@ typedef struct{
 	
 }sot_barrier_t;
 
+int sot_barrier_init(sot_barrier_t * barrier, int numberOfThreads);
+
+int sot_barrier_destroy(sot_barrier_t * barrier);
+
+int sot_barrier_wait(sot_barrier_t * barrier);
